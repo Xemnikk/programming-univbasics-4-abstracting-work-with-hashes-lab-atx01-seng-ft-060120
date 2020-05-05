a0378d968ba0937fe2c545a3dbb50f1586d59038
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-05 19:42:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   
-  if hash[key] =
-    hash[key] + 1.to_s
-  end
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
